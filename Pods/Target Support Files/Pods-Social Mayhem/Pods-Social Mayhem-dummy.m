f751bbf17247c3e80460726437a185d8c0902871
RustyBrown67 <apple@ians-mbp-2.home>
2017-04-13 23:16:04 +0100

    Initial Setup

---
+++ Pods/Target Support Files/Pods-Social Mayhem/Pods-Social Mayhem-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Social_Mayhem : NSObject
+@end
+@implementation PodsDummy_Pods_Social_Mayhem
+@end
---
+++ Pods/Target Support Files/Pods-Social Mayhem/Pods-Social Mayhem-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Social_MayhemVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Social_MayhemVersionString[];
+
