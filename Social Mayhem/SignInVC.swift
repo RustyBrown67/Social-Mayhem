e5adafc1fd1dfb5eb89677d9b40841a8adc88c8e
RustyBrown67 <apple@ians-mbp-2.home>
2017-04-14 17:32:00 +0100

    FB Authentication Completed

--- Social Mayhem/SignInVC.swift
+++ Social Mayhem/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -15,11 +18,32 @@ class SignInVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func facebookBtnTapped(_ sender: UIButton) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to authenticate with FB - \(error!)")
+            } else if result?.isCancelled == true {
+                print("User cancelled the authentication")
+            } else {
+                print("FB Authentication Successful")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+        
     }
-
+    
+    func firebaseAuthenticate(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase - \(error!)")
+            } else {
+                print("Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
